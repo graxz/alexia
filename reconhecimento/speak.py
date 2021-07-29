7f3876ec19598f310ea3056f27dff98f4963fe95
Isaddora Freitas <isaddorafreitasar@gmail.com>
2021-07-29 15:52:02 -0300

    adjust paths + add read a audio

---
+++ reconhecimento/speak.py
@@ -0,0 +1,13 @@
+import speech_recognition as sr
+
+recon = sr.Recognizer()
+
+with sr.Microphone() as source:
+
+    while True:
+        recon.adjust_for_ambient_noise(source)
+
+        print("Aguardando conexão...")
+        audio = recon.listen(source)
+
+        print(recon.recognize_sphinx(audio))
--- reconhecimento/sr.py
+++
@@ -1,8 +0,0 @@
-import speech_recognition as sr
-
-recon = sr.Recognizer()
-
-with sr.Microphone() as source:
-    print("Aguardando conexão...")
-    audio = recon.listen(source)
-    print(recon.recognize_sphinx(audio))
